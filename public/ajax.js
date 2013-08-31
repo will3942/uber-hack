6d228849e25dd3a3585fb422cf7f7826e3b61598
Josh Balfour <beakybal4@gmail.com>
2013-08-31 17:32:37 +0100

    Modified ajax lib, started enabling location links

--- public/ajax.js
+++ public/ajax.js
@@ -1,1 +1,66 @@
-function microAjax(B,A){this.bindFunction=function(E,D){return function(){return E.apply(D,[D])}};this.stateChange=function(D){if(this.request.readyState==4){this.callbackFunction(this.request.responseText)}};this.getRequest=function(){if(window.ActiveXObject){return new ActiveXObject("Microsoft.XMLHTTP")}else{if(window.XMLHttpRequest){return new XMLHttpRequest()}}return false};this.postBody=(arguments[2]||"");this.callbackFunction=A;this.url=B;this.request=this.getRequest();if(this.request){var C=this.request;C.onreadystatechange=this.bindFunction(this.stateChange,this);if(this.postBody!==""){C.open("POST",B,true);C.setRequestHeader("X-Requested-With","XMLHttpRequest");C.setRequestHeader("Content-type","application/x-www-form-urlencoded");C.setRequestHeader("Connection","close")}else{C.open("GET",B,true)}C.send(this.postBody)}};
+/*
+Copyright (c) 2008 Stefan Lange-Hegermann
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+function get(url, callbackFunction,errorCatcher)
+{
+	this.bindFunction = function (caller, object) {
+		return function() {
+			return caller.apply(object, [object]);
+		};
+	};
+
+	this.stateChange = function (object) {
+		if (this.request.readyState==4){
+			if (req.status == 200){
+				this.callbackFunction(this.request.responseText);
+			} else {
+				this.errorCatcher(req.status,this.request.responseText);
+			}
+		}
+			
+	};
+
+	this.getRequest = function() {
+		if (window.ActiveXObject)
+			return new ActiveXObject('Microsoft.XMLHTTP');
+		else if (window.XMLHttpRequest)
+			return new XMLHttpRequest();
+		return false;
+	};
+
+
+	this.callbackFunction=callbackFunction;
+	this.errorCatcher = errorCatcher;
+	this.url=url;
+	this.request = this.getRequest();
+	
+	if(this.request) {
+		var req = this.request;
+		req.onreadystatechange = this.bindFunction(this.stateChange, this);
+
+		
+		req.open("GET", url, true);
+		
+
+		req.send("");
+	}
+}
